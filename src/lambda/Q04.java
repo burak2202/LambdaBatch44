e1e9bd37f7a35f1d74bef472589fc5b183bd90fa
mrvtnc <mrvgul258@gmail.com>
2022-01-04 17:17:17 +0300

    son

--- src/lambda/Q04.java
+++ src/lambda/Q04.java
@@ -1,5 +1,9 @@
 package lambda;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Q04 {
     //        3) "Ali", "John", "Ali", "Alexander", "Taylor", "Ali", "Jackson" öğelerini içeren bir Dize listesi oluşturun
 //        4) "J" ile baslayin olan öğeleri almayın
@@ -9,4 +13,22 @@ public class Q04 {
 //        8) Konsoldaki öğeleri yazdırın
 //        9)Fonksiyonel Programlama'yı kullanın
 //        */
+
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("Ali");
+        list.add("John");
+        list.add("Ali");
+        list.add("Alexander");
+        list.add("Taylor");
+        list.add("Ali");
+        list.add("Jackson");
+
+        list.stream()
+                .filter(t->t.startsWith("J"))//j ile baslayanlari
+                .distinct()//farkli olanlari aldik
+                .sorted(Comparator.comparing(t->t.length())) //karakter sayisina gore natural sıra
+                .map(t->t.toUpperCase())
+                .forEach(t-> System.out.println(t));
+    }
 }
